674a779f28fc3b736929262deca309667c901b43
yuki <yukicharlishii@gmail.com>
2018-05-21 19:52:18 +0900

    Update forminput component

--- src/components/forminput.js
+++ src/components/forminput.js
@@ -7,22 +7,30 @@ import {
 } from 'reactstrap';
 
 const FormInput = (props) => (
-    <FormGroup>
+    <FormGroup className={ `w-100 ${props.className}` }>
         <Input
+            type={ props.type }
+            name={ props.name }
+            value={ props.value }
             placeholder={ props.placeholder }
             onChange={ props.onChange }
             invalid={ props.error ? true : false }
         />
-        <FormFeedback>
+        <FormFeedback className={ props.errorClassName }>
             { props.error }
         </FormFeedback>
     </FormGroup>
 );
 
 FormInput.propTypes = {
+    className: PropTypes.string,
+    type: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string,
     placeholder: PropTypes.string,
     onChange: PropTypes.func.isRequired,
     error: PropTypes.string,
+    errorClassName: PropTypes.string,
 };
 
 export default FormInput;
