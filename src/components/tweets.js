4e4bd263c29d0d837d574a63a2e2086020d2c6db
yuki <yukicharlishii@gmail.com>
2018-05-22 18:48:14 +0900

    Refactor code for building tweet items

--- src/components/tweets.js
+++ src/components/tweets.js
@@ -1,47 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-    ListGroup,
-} from 'reactstrap';
 
 import Tweet from './tweet';
 import UpdateTweetForm from './updatetweetform';
 
 const Tweets = (props) => {
-    let tweets = props.tweets;
-    let username = props.username;
+    const username = props.username;
+    const tweets = props.tweets;
+    const tweetItems = tweets.map(
+        tweet => (
+            tweet.user === username ? (
+                <UpdateTweetForm
+                    key={ tweet.id }
+                    data={ tweet }
+                    onClickTweet={
+                        () => props.onClickTweet(tweet)
+                    }
+                    onEditTweet={ props.onEditTweet }
+                    onDeleteTweet={
+                        () => props.onDeleteTweet(tweet)
+                    }
+                />
+            ) : (
+                <Tweet
+                    key={ tweet.id }
+                    data={ tweet }
+                    onClickTweet={
+                        () => props.onClickTweet(tweet)
+                    }
+                />
+            )
+        )
+    );
 
     return (
         <div className='flex-column p-3 tweets-container'>
-            <ListGroup>
-                {
-                    tweets.map(
-                        tweet => (
-                            tweet.user === username ? (
-                                <UpdateTweetForm
-                                    key={ tweet.id }
-                                    data={ tweet }
-                                    onClickTweet={
-                                        () => props.onClickTweet(tweet)
-                                    }
-                                    onEditTweet={ props.onEditTweet }
-                                    onDeleteTweet={
-                                        () => props.onDeleteTweet(tweet)
-                                    }
-                                />
-                            ) : (
-                                <Tweet
-                                    key={ tweet.id }
-                                    data={ tweet }
-                                    onClickTweet={
-                                        () => props.onClickTweet(tweet)
-                                    }
-                                />
-                            )
-                        )
-                    )
-                }
-            </ListGroup>
+            { tweetItems }
         </div>
     );
 };
